6db66612bd942034dc9bd799c9e8a789ab38a80e
Fantox <shubhapratimbiswas@gmail.com>
2022-04-19 21:02:24 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -92,10 +92,10 @@ export default class MessageHandler {
 		);
 		if (!command)
 			return void M.reply(
-				`No such command, Baka! Have you never seen someone use the command *${this.client.config.prefix}help*.`
+				`Ara ara, there is no such command programmed, Baka! Pls use the command *${this.client.config.prefix}help*.`
 			);
 		const user = await this.client.getUser(M.sender.jid);
-		if (user.ban) return void M.reply("You're Banned from using commands.");
+		if (user.ban) return void M.reply("Ara ara, looks like you're Banned from using commands.");
 		const state = await this.client.DB.disabledcommands.findOne({
 			command: command.config.command,
 		});
@@ -106,16 +106,16 @@ export default class MessageHandler {
 				}`
 			);
 		if (!command.config?.dm && M.chat === "dm")
-			return void M.reply("This command can only be used in groups");
+			return void M.reply("Ara ara, this command can only be used in Groups.");
 		if (
 			command.config?.modsOnly &&
 			!this.client.config.mods?.includes(M.sender.jid)
 		) {
-			return void M.reply(`Only MODS are allowed to use this command.`);
+			return void M.reply(`Ara ara, only MODS are allowed to use this command.`);
 		}
 		if (command.config?.adminOnly && !M.sender.isAdmin)
 			return void M.reply(
-				`This command is only meant for the group admins, Baka!`
+				`Ara ara, only group Admins can use this command, Baka!`
 			);
 		try {
 			await command.run(M, this.parseArgs(args));
