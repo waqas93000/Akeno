1e79afe0c4654aafd4f59e886752bfbf9f8526f9
Fantox <shubhapratimbiswas@gmail.com>
2022-04-19 21:16:32 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,12 +29,12 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const images = [
-			"https://wallpapercave.com/wp/wp3144753.jpg",
-			"https://wallpapercave.com/wp/wp4782018.jpg",
-			"https://wallpaperaccess.com/full/1326836.jpg",
-			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
-			"https://data.whicdn.com/images/304776416/original.jpg",
-			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
+			"https://wallpapercave.com/wp/wp9428151.jpg",
+			"https://wallpapercave.com/wp/wp9428155.png",
+			"https://wallpapercave.com/wp/wp7932407.jpg",
+			"https://wallpapercave.com/wp/wp7932387.jpg",
+			"https://wallpapercave.com/wp/wp5145520.jpg",
+			"https://wallpapercave.com/wp/wp8544651.jpg",
 		];
 		const selected = images[Math.floor(Math.random() * images.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -45,7 +45,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*🌟「 CHITOGE BROADCAST 」🌟*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `*⚡「 Akeno Broadcast 」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
 				caption: `${text}`,
 				contextInfo: {
