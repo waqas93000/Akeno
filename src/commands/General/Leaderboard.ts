825207e6223ee82141a8633f83166986487d2453
Fantox <shubhapratimbiswas@gmail.com>
2022-04-19 22:50:06 +0530

    Update Leaderboard.ts

--- src/commands/General/Leaderboard.ts
+++ src/commands/General/Leaderboard.ts
@@ -71,7 +71,7 @@ export default class Command extends BaseCommand {
       } else if (exp < 100000) {
         role = "💎 Supreme";
       } else {
-        role = "❄️ Mystic";
+        role = "☠️ Demon Lord";
       }
       let level: number;
       if (exp < 500) {
@@ -103,7 +103,7 @@ export default class Command extends BaseCommand {
     }
     return void M.reply(
       await this.client.getBuffer(
-        "https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg"
+        "https://wallpapercave.com/wp/wp9428151.jpg"
       ),
       MessageType.image,
       undefined,
