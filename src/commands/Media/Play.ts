54dfa0cef82cad109a397b38f14cd707031f9d8f
Fantox <shubhapratimbiswas@gmail.com>
2022-04-20 10:02:55 +0530

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -40,7 +40,7 @@ export default class Command extends BaseCommand {
             body: `Author : ${videos[0].author.name.substr(
               0,
               20
-            )}\n🌟 Chitoge 🌟`,
+            )}\n⚡ Akeno ⚡`,
             mediaType: 2,
             thumbnail: await this.client.getBuffer(
               `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`
