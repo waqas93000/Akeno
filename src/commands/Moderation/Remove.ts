6431d2dbfa3b02f6a27cc5ce047282fa7b60c307
Fantox <shubhapratimbiswas@gmail.com>
2022-04-20 11:29:06 +0530

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         let text = '*Action*\n\n'
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`How can I remove someone without being an admin?`)
+            return void M.reply(`Ara ara, How can I remove someone without being an admin?`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
