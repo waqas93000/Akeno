9dbd819edb58a3250a1e7d67caad5c7b7c59ddaf
Fantox <shubhapratimbiswas@gmail.com>
2022-04-20 10:38:28 +0530

    Update Delete.ts

--- src/commands/Moderation/Delete.ts
+++ src/commands/Moderation/Delete.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
         if (!M?.quoted?.message) return void M.reply('Quote the message you want to delete')
         if (M.quoted.sender !== this.client.user.jid)
 					return void M.reply(
-						`Do you want me to delete the message of a random member?`
+						`Ara ara, i can only delete messages sent by me.`
 					);
         await this.client.deleteMessage(M.from, {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
