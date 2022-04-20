211b04fc3fb51382bf1165f8931e4c54545cd3ca
Fantox <shubhapratimbiswas@gmail.com>
2022-04-20 12:01:48 +0530

    Update Rpaper.ts

--- src/commands/Weeb/Rpaper.ts
+++ src/commands/Weeb/Rpaper.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`🌟 Here you go.\n`,
+					`⚡ Here you go.\n`,
 					undefined
 				).catch((e) => {
 					console.log(
