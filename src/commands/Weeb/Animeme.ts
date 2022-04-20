1d48553d255a5dc558a12ea3fcffab0b7cdbe41f
Fantox <shubhapratimbiswas@gmail.com>
2022-04-20 12:03:00 +0530

    Update Animeme.ts

--- src/commands/Weeb/Animeme.ts
+++ src/commands/Weeb/Animeme.ts
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `Here you go ✨\n`,
+                    `⚡ Here you go ✨\n`,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
